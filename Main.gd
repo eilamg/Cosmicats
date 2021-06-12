39f5a0f3c2103bb9518f203409ebcca937f53d52
Eilam Gilboa <eilam.gilboa@gmail.com>
2021-06-12 15:35:55 -0700

    Update main

--- Main.gd
+++ Main.gd
@@ -4,10 +4,19 @@ extends Node2D
 var Star = preload("res://star/Star.tscn")
 
 
+var stages = [['Fish'], ['Octopus']]
+
+
 func _ready():
     randomize()
     spawn_star_groups()
-    spawn_random_stars()
+    fade_in_constellations()
+    spawn_random_stars(100)
+
+
+func fade_in_constellations():
+    $Constellations/Fish/AnimationPlayer.play("silhouette_fade_in")
+    $Constellations/Octopus/AnimationPlayer.play("silhouette_fade_in")
 
 
 func spawn_star_groups():
@@ -15,9 +24,13 @@ func spawn_star_groups():
     star_group.name = 'Fish'
     $Parallax/Sky/Stars.add_child(star_group)
 
+    star_group = StarGroup.new($Constellations/Octopus, Vector2(200 + randf() * 3440, 200 + randf() * 400), randf() * PI, randf() > 0.5)
+    star_group.name = 'Octopus'
+    $Parallax/Sky/Stars.add_child(star_group)
 
-func spawn_random_stars():
-    for i in range(100):
+
+func spawn_random_stars(n):
+    for i in range(n):
         var s = Star.instance()
         s.position = Vector2(randf() * 3840, 100 + randf() * 1080)
         s.name = 'Star'
