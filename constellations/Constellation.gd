86dab81ca54b816a5283a9e4e5a2662fc2fac7cc
Eilam Gilboa <eilam.gilboa@gmail.com>
2021-06-12 15:31:13 -0700

    Add proper constellation state transitions

--- constellations/Constellation.gd
+++ constellations/Constellation.gd
@@ -1,42 +1,44 @@
-tool
+# tool
 extends Node2D
 class_name Constellation
 
 
-var _is_ready = false
+# var _is_ready = false
 
 onready var axis = $Axis
-onready var stars = $Axis/Stars
-onready var silhouette = $Axis/Silhouette
-onready var skeleton = $Axis/Skeleton
-onready var art = $Axis/Art
+# onready var stars = $Axis/Stars
+# onready var silhouette = $Axis/Silhouette
+# onready var skeleton = $Axis/Skeleton
+# onready var art = $Axis/Art
+
+var controllable = true
 
 var dragging = false
 var spinning = false
 var click_offset = Vector2.ZERO
 var angle_offset = 0
 
-enum {STARS=1, SILHOUETTE=2, SKELETON=4, ART=8}
-export(int, FLAGS, "Stars", "Silhouette", "Skeleton", "Art") var visibility_flags = ART setget set_visibility_flags
+# enum {STARS=1, SILHOUETTE=2, SKELETON=4, ART=8}
+# export(int, FLAGS, "Stars", "Silhouette", "Skeleton", "Art") var visibility_flags = ART setget set_visibility_flags
 
 
-func _ready():
-    _is_ready = true
-    set_visibility_flags(visibility_flags)
+# func _ready():
+#     _is_ready = true
+#     set_visibility_flags(visibility_flags)
 
 
-func set_visibility_flags(value):
-    visibility_flags = value
-
-    if _is_ready:
-        stars.visible = visibility_flags & STARS
-        silhouette.visible = visibility_flags & SILHOUETTE
-        skeleton.visible = visibility_flags & SKELETON
-        art.visible = visibility_flags & ART
+# func set_visibility_flags(value):
+#     visibility_flags = value
+#
+#     if _is_ready:
+#         stars.visible = visibility_flags & STARS
+#         silhouette.visible = visibility_flags & SILHOUETTE
+#         skeleton.visible = visibility_flags & SKELETON
+#         art.visible = visibility_flags & ART
 
 
 func _on_input_event(viewport, event, shape_idx):
-    if not Engine.editor_hint:
+    if controllable:
         if event is InputEventMouseButton:
             click_offset = get_local_mouse_position()
 
@@ -54,16 +56,15 @@ func _on_input_event(viewport, event, shape_idx):
 
 
 func _physics_process(delta):
-    if not Engine.editor_hint:
-        if dragging:
-            position = get_global_mouse_position() - click_offset
+    if dragging:
+        position = get_global_mouse_position() - click_offset
 
-        if spinning:
-            pass
-            axis.rotation = get_angle_to(get_global_mouse_position()) - angle_offset
+    if spinning:
+        pass
+        axis.rotation = get_angle_to(get_global_mouse_position()) - angle_offset
 
-            if Input.is_action_just_released("right_click"):
-                spinning = false
+        if Input.is_action_just_released("right_click"):
+            spinning = false
 
 
 func mirror():
@@ -75,3 +76,26 @@ func get_star_positions():
     for star in $Axis/StarMarkers.get_children():
         star_positions.append(star.position)
     return star_positions
+
+
+func _on_correctly_positioned(star_group):
+    disable_player_control()
+    align_with_stargroup(star_group)
+
+
+func disable_player_control():
+    controllable = false
+
+
+func align_with_stargroup(star_group):
+    var tween = Tween.new()
+    add_child(tween)
+    tween.interpolate_property(self, "position", null, star_group.position, 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+    var delta_angle = fmod(star_group.rotation - axis.rotation, PI)
+    tween.interpolate_property(axis, "rotation", null, axis.rotation + delta_angle, 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+    tween.connect("tween_all_completed", self, "_on_aligned")
+    tween.start()
+
+
+func _on_aligned():
+    $AnimationPlayer.play("silhouette_to_skeleton")
--- constellations/StarGroup.gd
+++ constellations/StarGroup.gd
@@ -3,15 +3,19 @@ class_name StarGroup
 
 
 var Star = preload("res://star/Star.tscn")
-#var constellation # : Constellation
-#
-#
-
 var constellation = null
+
+
+signal correctly_positioned(star_group)
+
+
+const DISTANCE_THRESHOLD = 100
+const ANGLE_THRESHOLD = deg2rad(10)
 
 
 func _init(constellation_, pos=Vector2.ZERO, rot=0, mirrored=false):
     constellation = constellation_
+    connect("correctly_positioned", constellation, "_on_correctly_positioned")
     position = pos
     rotation = rot
     if mirrored:
@@ -27,3 +31,20 @@ func spawn_stars(star_positions):
         var s = Star.instance()
         s.position = pos
         add_child(s)
+
+
+func _process(delta):
+    if is_close_enough():
+        emit_signal("correctly_positioned", self)
+        set_process(false)
+
+
+func is_close_enough():
+    var delta_position = (constellation.position - position).length()
+    var delta_rotation = abs(fmod(constellation.axis.rotation - rotation, 2*PI))
+    var mirrored = not(constellation.axis.scale.x == scale.x)
+    var is_dragging = constellation.dragging
+    var is_spinning = constellation.spinning
+
+    # prints(delta_position, rad2deg(delta_rotation), mirrored, is_dragging, is_spinning)
+    return (delta_position <= DISTANCE_THRESHOLD) and (delta_rotation <= ANGLE_THRESHOLD) and not(mirrored) and not(is_dragging) and not(is_spinning)
