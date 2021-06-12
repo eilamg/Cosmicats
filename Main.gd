1d2e18b7b6633b887b83945752f1b8fd7f0bab2c
Eilam Gilboa <eilam.gilboa@gmail.com>
2021-06-11 23:28:09 -0700

    Update main

--- Main.gd
+++ Main.gd
@@ -2,4 +2,7 @@ extends Node2D
 
 
 func _ready():
-    Input.set_use_accumulated_input(false)
+    randomize()
+    var star_group = StarGroup.new($Constellations/Fish, Vector2(100 + randf() * 900, 100 + randf() * 500), randf() * PI, randf() > 0.5)
+    star_group.name = 'Fish'
+    $Stars.add_child(star_group)
