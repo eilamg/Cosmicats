f35a14788487babb259921a99483824bb1a7837e
Eilam Gilboa <eilam.gilboa@gmail.com>
2021-06-12 18:03:10 -0700

    Remove deprecated constellation visibility flags

--- constellations/Constellation.gd
+++ constellations/Constellation.gd
@@ -6,10 +6,6 @@ class_name Constellation
 # var _is_ready = false
 
 onready var axis = $Axis
-# onready var stars = $Axis/Stars
-# onready var silhouette = $Axis/Silhouette
-# onready var skeleton = $Axis/Skeleton
-# onready var art = $Axis/Art
 
 var controllable = true
 
@@ -17,24 +13,6 @@ var dragging = false
 var spinning = false
 var click_offset = Vector2.ZERO
 var angle_offset = 0
-
-# enum {STARS=1, SILHOUETTE=2, SKELETON=4, ART=8}
-# export(int, FLAGS, "Stars", "Silhouette", "Skeleton", "Art") var visibility_flags = ART setget set_visibility_flags
-
-
-# func _ready():
-#     _is_ready = true
-#     set_visibility_flags(visibility_flags)
-
-
-# func set_visibility_flags(value):
-#     visibility_flags = value
-#
-#     if _is_ready:
-#         stars.visible = visibility_flags & STARS
-#         silhouette.visible = visibility_flags & SILHOUETTE
-#         skeleton.visible = visibility_flags & SKELETON
-#         art.visible = visibility_flags & ART
 
 
 func _on_input_event(viewport, event, shape_idx):
